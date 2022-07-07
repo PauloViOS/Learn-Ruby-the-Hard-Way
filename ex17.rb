2fefa4e733f6c00813e578c198dea4fea317e74a
Paulo Santos <paulo.santos@buser.com.br>
2022-07-07 00:46:56 -0300

    Added small commentary to ex17

--- ex17.rb
+++ ex17.rb
@@ -2,6 +2,7 @@ from_file, to_file = ARGV
 
 puts "Copying from #{from_file} to #{to_file}"
 
+# reading the file like this prevents us from having to open and close it explicitly
 in_file = File.read(from_file)
 
 puts "The input file is #{in_file.length} chars long"
